fd35c34eb63140969ecd35aab83e0cc597fea6f6
591317 <69850816+591317@users.noreply.github.com>
2024-02-07 15:07:14 +0100

    need to double check that the images are not duplicates after each other. add leave button for the game to end

--- app/src/main/java/com/example/oblig1_quizapp/QuizGame.java
+++ app/src/main/java/com/example/oblig1_quizapp/QuizGame.java
@@ -2,8 +2,11 @@ package com.example.oblig1_quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -16,8 +19,17 @@ import java.util.Random;
 
 public class QuizGame extends AppCompatActivity {
 
-    // list of Uri a variable that is used for saveing the content that is sent form gallery intent
+    // Counters for the Correct answer and Attempts tried
+    int correctAnswerCount = 0;
+    int attemptsCount = 0;
+
+    int previousImage = -1;
+
+    // list of Uri a variable that is used for saving the content that is sent form gallery intent
     List<Uri> selectedImageUris = new ArrayList<>();
+
+    // The randomUri that is put in the ImageView and shared outside the updateUi method
+    Uri randomUriA;
 
     // ImageView for picture displayed
     ImageView imageView;
@@ -51,7 +63,9 @@ public class QuizGame extends AppCompatActivity {
         buttonB = findViewById(R.id.answerB);
         buttonC = findViewById(R.id.answerC);
 
-        if (!selectedImageUris.isEmpty()){
+        updateUI(selectedImageUris);
+
+        /*if (!selectedImageUris.isEmpty()){
             // make a list where we have control of the random int values so we can shuffle the values
             List<Uri> randomUriList = new ArrayList<>();
             // generate a random-number so we can get a random Uri and display that image
@@ -88,13 +102,169 @@ public class QuizGame extends AppCompatActivity {
             //set the button values from the indexes of the shuffled random-list
             buttonA.setText(FileUtils.getFileNameFromUri(randomUriList.get(0)));
             buttonB.setText(FileUtils.getFileNameFromUri(randomUriList.get(1)));
-            buttonC.setText(FileUtils.getFileNameFromUri(randomUriList.get(2)));
+            buttonC.setText(FileUtils.getFileNameFromUri(randomUriList.get(2)));*/
+
+            // add a on click listener to the buttons to check if the text content of the button is correct and then change the collor
+            buttonA.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // retrieving the text value of the button and convert it to a string
+                   String buttonTextValue = buttonA.getText().toString();
+
+                    // check if the value is equal to the picture name
+                    if (buttonTextValue.equals(FileUtils.getFileNameFromUri(randomUriA))){
+                        buttonA.setBackgroundColor(Color.GREEN);
+                        correctAnswerCount ++;
+                        attemptsCount ++;
+
+                        // set the values of the counters to the textViews to display the number of correct answers and attempts
+                        correctAnswer.setText("Correct Answer: " + correctAnswerCount);
+                        attempts.setText("Attempts: " + attemptsCount);
+
+                        updateUI(selectedImageUris);
+                    }else{
+                        buttonA.setBackgroundColor(Color.RED);
+                        attemptsCount ++;
+
+                        // set the values of the counters to the textViews to display the number of correct answers and attempts
+                        attempts.setText("Attempts: " + attemptsCount);
+                    }
+
+
+
+                }
+            });
+
+            buttonB.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // retrieving the text value of the button and convert it to a string
+                    String buttonTextValue = buttonB.getText().toString();
+
+                    // check if the value is equal to the picture name
+                    if (buttonTextValue.equals(FileUtils.getFileNameFromUri(randomUriA))){
+                        buttonB.setBackgroundColor(Color.GREEN);
+                        correctAnswerCount ++;
+                        attemptsCount ++;
+
+                        // set the values of the counters to the textViews to display the number of correct answers and attempts
+                        correctAnswer.setText("Correct Answer: " + correctAnswerCount);
+                        attempts.setText("Attempts: " + attemptsCount);
+
+                        updateUI(selectedImageUris);
+                    }else{
+                        buttonB.setBackgroundColor(Color.RED);
+                        attemptsCount ++;
+
+                        // set the values of the counters to the textViews to display the number of correct answers and attempts
+                        attempts.setText("Attempts: " + attemptsCount);
+                    }
+
+
+
+                }
+            });
+
+            buttonC.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // retrieving the text value of the button and convert it to a string
+                    String buttonTextValue = buttonC.getText().toString();
+
+                    // check if the value is equal to the picture name
+                    if (buttonTextValue.equals(FileUtils.getFileNameFromUri(randomUriA))){
+                        buttonC.setBackgroundColor(Color.GREEN);
+                        correctAnswerCount ++;
+                        attemptsCount ++;
+
+                        // set the values of the counters to the textViews to display the number of correct answers and attempts
+                        correctAnswer.setText("Correct Answer: " + correctAnswerCount);
+                        attempts.setText("Attempts: " + attemptsCount);
+
+                        updateUI(selectedImageUris);
+
+                    }else{
+                        buttonC.setBackgroundColor(Color.RED);
+                        attemptsCount ++;
+
+                        // set the values of the counters to the textViews to display the number of correct answers and attempts
+                        attempts.setText("Attempts: " + attemptsCount);
+                    }
+
+
+
+                }
+            });
+
+
 
 
         }
 
+    // method to call for updating the ImageView and the buttons when correct answers is clicked
+    public void updateUI(List<Uri> selectedImageUris){
+
+        if (!selectedImageUris.isEmpty()){
+
+            // make a list where we have control of the random int values so we can shuffle the values
+            List<Uri> randomUriList = new ArrayList<>();
+            // generate a random-number so we can get a random Uri and display that image
+            Random random = new Random();
+
+            int randomIndexA, randomIndexB, randomIndexC;
+
+            do{
+                randomIndexA = random.nextInt(selectedImageUris.size()); // choosing a random int from the size range of the list
+            } while (randomIndexA == previousImage); // if they are equal retrieve a new random int from the list
+
+            previousImage = randomIndexA;
+
+            //make sure that the random number are not equal
+            do{
+                randomIndexB = random.nextInt(selectedImageUris.size());
+            } while (randomIndexB == randomIndexA); // if they are equal retrieve a new random int from the list.
+
+            do {
+                randomIndexC = random.nextInt(selectedImageUris.size());
+            } while (randomIndexC == randomIndexA || randomIndexC == randomIndexB); // if they are equal retrieve a new random int from the list.
 
 
+            // get the random Uri from the selectedImageUris list
+            randomUriA = selectedImageUris.get(randomIndexA);
+            Uri randomUriB = selectedImageUris.get(randomIndexB);
+            Uri randomUriC = selectedImageUris.get(randomIndexC);
 
+            // adding the randomIndexes to the new list so we can shuffle them so the buttons has different answer all the time
+            randomUriList.add(randomUriA);
+            randomUriList.add(randomUriB);
+            randomUriList.add(randomUriC);
+
+            // set the random Uri in the imageView
+            imageView.setImageURI(randomUriA);
+
+            // us Collection to shuffle the list to make the correct answer appear a different place each time
+            Collections.shuffle(randomUriList);
+
+            //set the button values from the indexes of the shuffled random-list
+            buttonA.setText(FileUtils.getFileNameFromUri(randomUriList.get(0)));
+            buttonB.setText(FileUtils.getFileNameFromUri(randomUriList.get(1)));
+            buttonC.setText(FileUtils.getFileNameFromUri(randomUriList.get(2)));
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    // reset the button collors when the method is loaded in again
+                    resetButtonCollor();
+                }
+            }, 800);
     }
+}
+
+ public void resetButtonCollor(){
+        buttonA.setBackgroundColor(Color.rgb(106, 13, 174));
+        buttonB.setBackgroundColor(Color.rgb(106, 13, 174));
+        buttonC.setBackgroundColor(Color.rgb(106, 13, 174));
+ }
+
 }
